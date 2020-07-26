f597a937bf1cc4a11e53d6b340b6610dbf6391ab
luisrodrigoads <luisrodrigoads@gmail.com>
2020-07-26 00:30:57 -0300

    reconfigure and better render guatering list

--- source/Gathering.js
+++ source/Gathering.js
@@ -11,22 +11,47 @@ export default function Gathering() {
   const navigation = useNavigation()
   const [ districts, setDistricts ] = useState([]);
   const [ selected, setSelected ] = useState();
-
+  
   async function getDistrictsFromApi() {
  
     try {
       const response = await axios.get(URL_API.coleta);  
-      setDistricts(response.data);
-      console.log(response.data);
+     
+      setDistricts(group_by(response.data,'neighborhood').map(function (item){
+        return {key: item.key,values:group_by(item.values,'type')};
+      }));
+      
     } catch (error) {
       console.log(error);
     }
   }
 
+  function group_by (lista, coluna) {
+    var colunas = {};
+    var resultado = [];
+  
+    lista.forEach(function (item) {
+      var reg = {};
+  
+      colunas[item[coluna]] = colunas[item[coluna]] || [];
+  
+      for (var i in item) 
+        if (i != coluna) 
+          reg[i] = item[i]; 
+  
+      colunas[item[coluna]].push(reg);
+    });
+  
+    for (var i in colunas) 
+      resultado.push({key: i, values: colunas[i]});
+  
+    return resultado;
+  }
+  
+
   useEffect(() => {
     getDistrictsFromApi();
   }, []);
-
 
   return (
     <Container>
@@ -47,7 +72,7 @@ export default function Gathering() {
       </Header>
       <Content style={styles.content}>
         {  
-          /*districts.length === 0 ? 
+          districts.length === 0 ? 
             <ActivityIndicator size='large' color='#529C52' style={{ paddingTop: 25 }}/>
             :
             <View style={styles.container}>
@@ -56,51 +81,18 @@ export default function Gathering() {
                   return (
                     <View key={index} style={styles.districtsListView}>
                       <Button style={styles.districtsListButton} onPress={() => { setSelected(index !== selected ? index : undefined) }}>
-                        <Text>{item.name}</Text>
+                        <Text>{item.key}</Text>
                       </Button>
                       {
                         index === selected ? 
                           <View style={styles.districtsListInsideView} >
                             <View style={styles.districtsListSelective}>
-                              <Text style={[styles.districtsListTexts, {color: '#1d814c', fontSize: 18, paddingBottom: 0}]}>Coleta Seletiva:</Text>
-                              <Text style={styles.districtsListTexts}>{item.selective}</Text>
+                              <Text style={[styles.districtsListTexts, {color: '#1d814c', fontSize: 18, paddingBottom: 0}]}>{item.values[0].key}</Text>
+                              <Text style={styles.districtsListTexts}>{item.values[0].values[0].description}</Text>
                             </View>
                             <View>
-                              <Text style={[styles.districtsListTexts, {color: '#1d814c', fontSize: 18, paddingBottom: 0,}]}>Coleta Orgânica:</Text>
-                              <Text style={styles.districtsListTexts}>{item.organic}</Text>
-                            </View>
-                          </View>
-                          :
-                          null
-                      }
-                    </View>
-                  ) 
-                })}
-            </View>*/
-
-            districts.length === 0 ? 
-            <ActivityIndicator size='large' color='#529C52' style={{ paddingTop: 25 }}/>
-            :
-            <View style={styles.container}>
-                <H1 style={[styles.title, {marginBottom: 5}]}>Selecione seu Bairro</H1>
-                {districts.map((item, index) => {
-                  return (
-                    <View key={index} style={styles.districtsListView}>
-                    
-                      <TouchableOpacity onPress={() => { setSelected(index !== selected ? index : undefined) }}>
-                          <View style={styles.districtsListButton}>
-                            <Text style={{fontSize:20,color:'white'}} >{item.neighborhood}</Text>
-                            {
-                              <Badge success style={{position:'absolute',top: 5,right: 5}} ><Text style={{color:'#fff',fontSize:18}} >{item.type == "organica" ? "organica" : "seletiva"}</Text></Badge>
-                            }
-                        </View>
-                      </TouchableOpacity>
-                      {
-                        index === selected ? 
-                          <View style={styles.districtsListInsideView} >
-                            <View style={styles.districtsListSelective}>
-                              <Text style={[styles.districtsListTexts, {color: '#1d814c', fontSize: 18, paddingBottom: 0}]}>{item.type}</Text>
-                              <Text style={styles.districtsListTexts}>{item.description}</Text>
+                              <Text style={[styles.districtsListTexts, {color: '#1d814c', fontSize: 18, paddingBottom: 0,}]}>{item.values[1].key}</Text>
+                              <Text style={styles.districtsListTexts}>{item.values[1].values[0].description}</Text>
                             </View>
                           </View>
                           :
@@ -110,7 +102,6 @@ export default function Gathering() {
                   ) 
                 })}
             </View>
-
         }
       </Content>
     </Container>
